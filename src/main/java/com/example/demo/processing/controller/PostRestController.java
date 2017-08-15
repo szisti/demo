412805fd201a35172a4f6d8f800d617596b8c22b
Istvan Szabo <istvan.szabo@betvictor.com>
2017-08-15 17:12:45 +0200

    Added output format

--- src/main/java/com/example/demo/processing/controller/PostRestController.java
+++ src/main/java/com/example/demo/processing/controller/PostRestController.java
@@ -8,12 +8,25 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 public class PostRestController {
     private static final Logger LOG = LoggerFactory.getLogger(PostRestController.class);
 
+    /**
+     * Takes updatgram
+     * looking for market set
+     * outputs a map of:
+     * market descriptions:
+     *  selection description: probability
+     *
+     * @param update
+     * @return
+     */
     @RequestMapping(value = "/data", method = RequestMethod.POST)
-    public void processData(@RequestBody Updategram update) {
+    public Map<String, Map<String, Double>> processData(@RequestBody Updategram update) {
         LOG.info(update.getHeader().getMessageGuid());
+        return null;
     }
 }
